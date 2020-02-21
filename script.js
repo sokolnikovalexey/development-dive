0918174a847378ada184f899fc915a1514980d86
Alex Sokolnikov <sokolnikov.alexey@gmail.com>
2020-02-21 09:36:32 +0300

    js animation

--- script.js
+++ script.js
@@ -2,26 +2,24 @@ var GAME = {
     width: 500,
     height: 500,
     fps: 1000 / 60,
-    canvasContext: null,
-    background: new Image()
+    canvasContext: null
 }
 
-var PLAYER = {
-    x: 50,
-    y: 0,
-    width: 20,
-    height: 100,
-    color: "green",
-    yDirection: 0,
-    speed: 40,
+var BALL = {
+    x: 100,
+    y: 100,
+    size: 20,
+    color: "red",
+    speed: 20,
+    xDirection: 4,
+    yDirection: 4
 }
 
 function init() {
     var canvas = document.getElementById("canvas");
     _initCanvas(canvas);
-    _initEventsListeners(canvas);
 
-    play();
+    setInterval(play, GAME.fps);  
 }
 
 function play() {
@@ -32,37 +30,18 @@ function play() {
 function draw() {
     GAME.canvasContext.clearRect(0, 0, GAME.width, GAME.height);
 
-    GAME.canvasContext.fillStyle = PLAYER.color;
-    GAME.canvasContext.fillRect(PLAYER.x, PLAYER.y, PLAYER.width, PLAYER.height); //Рисуем игрока
+    GAME.canvasContext.fillStyle = BALL.color;
+    GAME.canvasContext.fillRect(BALL.x, BALL.y, BALL.size, BALL.size); //Рисуем мячик
 }
 
 function update() {
-    //Двигаем игрока на его скорость
-    PLAYER.y += PLAYER.yDirection;
-    PLAYER.yDirection = 0;
+    // двигаем мячик на его скорость
+    BALL.x += BALL.xDirection;
+    BALL.y += BALL.yDirection;
 }
 
 function _initCanvas(canvas) {
     canvas.width = GAME.width;
     canvas.height = GAME.height;
     GAME.canvasContext = canvas.getContext("2d");
-}
-
-function _initEventsListeners(canvas) {
-    document.addEventListener("keydown", _onDocumentKeyDown);
-    canvas.addEventListener("mousemove", _onCanvasMouseMove);
-}
-
-function _onCanvasMouseMove(event) {
-    PLAYER.y = event.clientY;
-    play();
-}
-
-function _onDocumentKeyDown(event) {
-    if (event.key == "ArrowUp") {
-        PLAYER.yDirection = -PLAYER.speed;
-    } else if (event.key == "ArrowDown") {
-        PLAYER.yDirection = PLAYER.speed;
-    }
-    play();
 }
