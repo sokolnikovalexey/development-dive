d09a0b76d0a78d2a37187342cf14ac81c443a56d
Alex Sokolnikov <sokolnikov.alexey@gmail.com>
2020-02-21 09:39:05 +0300

    js draw

--- script.js
+++ script.js
@@ -1,43 +1,20 @@
 var GAME = {
     width: 500,
     height: 500,
-    fps: 1000 / 60,
     canvasContext: null
-}
-
-var BALL = {
-    x: 100,
-    y: 100,
-    size: 20,
-    color: "red",
-    speed: 20,
-    xDirection: 4,
-    yDirection: 4
 }
 
 function init() {
     var canvas = document.getElementById("canvas");
     _initCanvas(canvas);
-
-    setInterval(play, GAME.fps);  
-}
-
-function play() {
-    draw();
-    update();
+    draw(); 
 }
 
 function draw() {
     GAME.canvasContext.clearRect(0, 0, GAME.width, GAME.height);
 
-    GAME.canvasContext.fillStyle = BALL.color;
-    GAME.canvasContext.fillRect(BALL.x, BALL.y, BALL.size, BALL.size); //Рисуем мячик
-}
-
-function update() {
-    // двигаем мячик на его скорость
-    BALL.x += BALL.xDirection;
-    BALL.y += BALL.yDirection;
+    GAME.canvasContext.fillStyle = "red";
+    GAME.canvasContext.fillRect(10, 10, 50, 50);
 }
 
 function _initCanvas(canvas) {
