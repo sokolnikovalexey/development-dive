01ef20a2eef6775926a89c9c672f6f2c1b6b4040
sokolnikov.alexey <sokolnikov.alexey@gmail.com>
2020-02-22 11:02:14 +0300

    lw1 draw

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-var GAME = {
-    width: 500,
-    height: 500,
-    canvasContext: null
-}
+var canvasContext = null;
 
 function init() {
     var canvas = document.getElementById("canvas");
-    _initCanvas(canvas);
-    draw(); 
+    canvasContext = canvas.getContext("2d");
+    draw();
 }
 
 function draw() {
-    GAME.canvasContext.clearRect(0, 0, GAME.width, GAME.height);
-
-    GAME.canvasContext.fillStyle = "red";
-    GAME.canvasContext.fillRect(10, 10, 50, 50);
+    canvasContext.fillStyle = "green";
+    canvasContext.fillRect(20, 20, 50, 50);
+    canvasContext.fillStyle = "red";
+    canvasContext.fillRect(10, 10, 50, 50);
 }
 
-function _initCanvas(canvas) {
-    canvas.width = GAME.width;
-    canvas.height = GAME.height;
-    GAME.canvasContext = canvas.getContext("2d");
-}
