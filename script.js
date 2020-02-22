157d66ca34edb303314190d1b435cd6c1ca28673
sokolnikov.alexey <sokolnikov.alexey@gmail.com>
2020-02-22 12:03:50 +0300

    lw2 animation & logic

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
 var canvasContext = null;
 
+var game = {
+    fps: 1000/60,
+    width: 480,
+    height: 320
+}
+
+var ball = {
+    x: 0,
+    y: 0,
+    width: 50,
+    height: 50,
+    color: "red",
+    xDirection: 5,
+    yDirection: 5
+}
+
 function init() {
     var canvas = document.getElementById("canvas");
     canvasContext = canvas.getContext("2d");
+
+    setInterval(play, game.fps); 
+}
+
+function play() {
     draw();
+    update();
+}
+
+function update() {
+    // направляем шарик в противоположную сторону при соприкосновении с ПОЛОМ или ПОТОЛКОМ игрового поля
+    if (ball.y < 0 || (ball.y + ball.height > game.height)) {
+        ball.yDirection = -ball.yDirection;
+    }
+
+    // направляем шарик в противоположную сторону при соприкосновении с ПОЛОМ или ПОТОЛКОМ игрового поля
+    if (ball.x < 0 || (ball.x + ball.width > game.width)) {
+        ball.xDirection = -ball.xDirection;
+    }
+
+    ball.x = ball.x + ball.xDirection;
+    ball.y = ball.y + ball.yDirection;
 }
 
 function draw() {
-    canvasContext.fillStyle = "green";
-    canvasContext.fillRect(20, 20, 50, 50);
-    canvasContext.fillStyle = "red";
-    canvasContext.fillRect(10, 10, 50, 50);
+    canvasContext.clearRect(0, 0, game.width, game.height);
+    canvasContext.fillStyle = ball.color;
+    canvasContext.fillRect(ball.x, ball.y, ball.width, ball.height); 
 }
 
